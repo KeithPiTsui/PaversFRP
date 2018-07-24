9308d3d50aa17fa1e6aee044f61604344c2d4c7d
Keith <keith@KeithMacOS.local>
2018-07-24 18:40:26 +0800

    add Higher kinded type

--- Sources/PaversFRP/Protocols/Monoid.swift
+++ Sources/PaversFRP/Protocols/Monoid.swift
@@ -1,3 +1,6 @@
+/// A type is `Monoid` if it is `Semigroup` with an identity that
+/// combine this identity with other value of this type would
+/// result to that value.
 public protocol Monoid: Semigroup {
   static func identity () -> Self
 }
--- Sources/PaversFRP/Types/SumType/List.swift
+++ Sources/PaversFRP/Types/SumType/List.swift
@@ -7,10 +7,31 @@
 
 private typealias Lazy<A> = Arrow<(), A>
 
-
 public enum List<A> {
   case Nil
   indirect case Cons(A, () -> List<A>)
+}
+
+public func zipList<A, B, C>(xs: @escaping () -> List<A>,
+                             ys: @escaping () -> List<B>,
+                             f: @escaping (A, B) -> C) -> List<C> {
+  switch (xs(), ys()) {
+  case (.Cons(let x, let xrest), .Cons(let y, let yrest)):
+    return .Cons( f(x, y), {zipList(xs: xrest, ys: yrest, f: f)})
+  default:
+    return .Nil
+  }
+}
+
+public func tail<A> (xs: @escaping () -> List<A>) -> () -> List<A> {
+  switch xs() {
+  case .Cons(_, let rest): return rest
+  default: fatalError("List is empty")
+  }
+}
+
+public func fib() -> List<Int> {
+  return List<Int>.Cons(1, {List<Int>.Cons(1, {zipList(xs: fib, ys: tail(xs: fib), f: +)})})
 }
 
 public func head<A> (xs: List<A>) -> A {
@@ -29,29 +50,4 @@ public func infiniteOne() -> List<Int> {
 }
 
 
-public func zipList<A, B, C>(xs: @escaping () -> List<A>,
-                             ys: @escaping () -> List<B>,
-                             f: @escaping (A, B) -> C) -> List<C> {
-  switch (xs(), ys()) {
-  case (.Cons(let x, let xrest), .Cons(let y, let yrest)):
-    return .Cons( f(x, y), {zipList(xs: xrest, ys: yrest, f: f)})
-  default:
-    return .Nil
-  }
-}
 
-public func tail<A> (xs: @escaping () -> List<A>) -> () -> List<A> {
-  switch xs() {
-  case .Cons(_, let rest):
-    return rest
-  default:
-    fatalError("List is empty")
-  }
-}
-
-//public let infiniteOne : List<Int> = List<Int>.Cons(1, infiniteOne)
-public func fib() -> List<Int> {
-  return List<Int>.Cons(1,
-                        {List<Int>.Cons(1,
-                                        {zipList(xs: fib, ys: tail(xs: fib), f: +)})})
-}
